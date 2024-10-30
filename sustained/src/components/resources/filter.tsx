a9d00261d2a0373a54084572275552c1c8fd68a4
jtng189 <jtan189@uw.edu>
2024-10-29 20:06:19 -0700

    added filtering by resource type

--- sustained/src/components/resources/filter.tsx
+++ sustained/src/components/resources/filter.tsx
@@ -1,1 +1,28 @@
-export {}
+export type FilterData = {
+    resourceTypes: string[]
+    setSelectedResourceType: (resourceType: string) => void // unsure why you have to define it like this
+}
+
+export default function Filter(props: FilterData) {
+    const resourceTypes = props.resourceTypes
+
+    // sort by resource type
+    const handleResourceChange = (event: any) => {
+        const value = event.target.value;
+        props.setSelectedResourceType(value) // allows Library to filter
+    }
+
+    return (
+        <div>
+            <label htmlFor="resourceTypeFilter">Filter by Resource Type: </label>
+            <select id="resourceTypeFilter" onChange={handleResourceChange}>
+                <option value="">All</option>
+                {resourceTypes.map((type) => (
+                    <option key={type} value={type}>
+                        {type}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
--- sustained/src/components/resources/library.tsx
+++ sustained/src/components/resources/library.tsx
@@ -1,9 +1,14 @@
 import ResourceCard, { ResourceData } from './ResourceCard';
 import resourcesData from './resources.json'
+import { useState, useEffect, ReactNode } from 'react';
+import Filter from './Filter'
 
 function Library() {
     const resources: ResourceData[] = resourcesData as ResourceData[]
-    
+    // Test to filter by resource type
+    const [filteredResources, setFilteredResources] = useState<ReactNode>([])
+    const [selectedResourceType, setSelectedResourceType] = useState("")
+
     const resourceList = resources.map(resource => {
         return (
             <ResourceCard
@@ -15,10 +20,38 @@ function Library() {
         )
     })
 
+    // Gathers unique resource types
+    const resourceTypes = resources
+        .map(resource => resource.resource_type)
+        .filter((resourceType, index, self) => {
+            return self.indexOf(resourceType) === index;
+        })
+    
+        useEffect(() => {
+        // filters the resource list to the user's selection
+        if (selectedResourceType === "") {
+            setFilteredResources(resourceList)
+        } else {
+            const filteredData = resources
+                .filter(resource => resource.resource_type === selectedResourceType)
+                .map(filteredResource => (
+                    <ResourceCard
+                        title={filteredResource.title}
+                        resource_type={filteredResource.resource_type}
+                        description={filteredResource.description}
+                        published_date={filteredResource.published_date}
+                    />
+                ));
+
+            setFilteredResources(filteredData);
+        }
+    }, [selectedResourceType, resourceList, resources])
+
     return (
         <div>
-            <h1>Resource Library</h1>
-            {resourceList}
+            <h1>Resource Library</h1>   
+            <Filter resourceTypes={resourceTypes} setSelectedResourceType={setSelectedResourceType} />    
+            {filteredResources}
         </div>
     );
 }
