a5c85619be7e27a7563f3b61a8c2995725504cf8
kadebencomo <davidbencomo15@gmail.com>
2024-10-30 17:41:54 -0700

    included bootstrap framework and also started work towards register component

--- sustained/src/components/Register/register.tsx
+++ sustained/src/components/Register/register.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const register = () => {
+const Register = () => {
   return (
-    <div>
-      
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="card p-4">
+        <div className="text-center mb-4" >
+        <h1 className="text-center"> Register Now</h1>
+        </div>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" className="form-control" id="password"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Confirm Password</label>
+            <input type="password" className="form-control" id="password"/>
+          </div>
+          <button type="submit" className="btn btn-primary w-100 py-2">Register</button>
+        </form>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default register
+export default Register;
--- sustained/src/components/app/app.tsx
+++ sustained/src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Register from '../register/register';
 
-const app = () => {
+const App = () => {
   return (
     <div>
-      
+      <Register />
     </div>
   )
 }
 
-export default app
+export default App;
