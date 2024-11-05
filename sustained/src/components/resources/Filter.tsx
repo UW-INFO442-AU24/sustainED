0a0f1f98403f690025c168ed1c19358054c76cf5
jtng189 <jtan189@uw.edu>
2024-11-05 14:53:08 -0800

    added submit button for filter

--- sustained/src/components/resources/Filter.tsx
+++ sustained/src/components/resources/Filter.tsx
@@ -6,23 +6,29 @@ export type FilterData = {
 export default function Filter(props: FilterData) {
     const resourceTypes = props.resourceTypes
 
+    let selectedResourceType : string
+
     // sort by resource type
     const handleResourceChange = (event: any) => {
-        const value = event.target.value;
-        props.setSelectedResourceType(value) // allows Library to filter
+        selectedResourceType = event.target.value;
+    }
+
+    const submitFilter = (event: any) => {
+        props.setSelectedResourceType(selectedResourceType)
     }
 
     return (
         <div>
             <label htmlFor="resourceTypeFilter">Filter by Resource Type: </label>
             <select id="resourceTypeFilter" onChange={handleResourceChange}>
-                <option key="all" value="">All</option>
+                <option value="">All</option>
                 {resourceTypes.map((type) => (
                     <option key={type} value={type}>
                         {type}
                     </option>
                 ))}
             </select>
+            <button type="submit" onClick={submitFilter}>Filter</button>
         </div>
     );
 }
