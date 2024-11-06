a31b1ff9dd5897fb46b6a0978f8cfba43a07eae9
jtng189 <jtan189@uw.edu>
2024-11-05 20:58:40 -0800

    added multiple filtering options to data

--- sustained/src/components/resources/Library.tsx
+++ sustained/src/components/resources/Library.tsx
@@ -4,51 +4,70 @@ import { useState, useEffect, ReactNode } from 'react';
 import Filter from './Filter'
 
 function Library() {
-    const resources: ResourceData[] = resourcesData as ResourceData[]
-    // Test to filter by resource type
+    const resources: ResourceData[] = resourcesData as ResourceData[]  
+
     const [filteredResources, setFilteredResources] = useState<ReactNode>([])
-    const [selectedResourceType, setSelectedResourceType] = useState("")
+    const [selectedResourceType, setSelectedResourceType] = useState<String>("")
+    const [selectedEnvironmentTopic, setSelectedEnvironmentTopic] = useState<String>("")
+    const [selectedGradeLevel, setSelectedGradeLevel] = useState<number>(0)
 
-    const resourceList = resources.map(resource =>
-        <ResourceCard
-            title={resource.title}
-            resource_type={resource.resource_type} 
-            description={resource.description} 
-            published_date={resource.published_date}                
-        />
-    )
 
-    // Gathers unique resource types
+    // Gathers unique resource types, environment topic, and grade levels
     const resourceTypes: string[] = resources
         .map(resource => resource.resource_type)
         .filter((resourceType, index, self) => {
             return self.indexOf(resourceType) === index;
         })
     
-    useEffect(() => {
-        // filters the resource list to the user's selection
-        if (selectedResourceType === "") {
-            setFilteredResources(resourceList)
-        } else {
-            const filteredData = resources
-                .filter(resource => resource.resource_type === selectedResourceType)
-                .map(filteredResource => (
-                    <ResourceCard
-                        title={filteredResource.title}
-                        resource_type={filteredResource.resource_type}
-                        description={filteredResource.description}
-                        published_date={filteredResource.published_date}
-                    />
-                ));
+    const environment_topics: string[] = resources
+        .map(resource => resource.environment_topic)
+        .filter((environment_topic, index, self) => {
+            return self.indexOf(environment_topic) === index;
+        })
 
-            setFilteredResources(filteredData);
+    const grade_levels: number[] = resources
+        .map(resource => resource.grade_level)
+        .filter((grade_level, index, self) => {
+            return self.indexOf(grade_level) === index;
+        })
+
+    useEffect(() => {
+        let filteredData : ResourceData[] = resources
+
+        if (selectedResourceType) {
+            filteredData = filteredData.filter(resource => resource.resource_type === selectedResourceType);
         }
-    }, [selectedResourceType])
+        
+        if (selectedEnvironmentTopic) {
+            filteredData = filteredData.filter(resource => resource.environment_topic === selectedEnvironmentTopic);
+        }
+        
+        if (selectedGradeLevel > 0) {
+            filteredData = filteredData.filter(resource => resource.grade_level === selectedGradeLevel);
+        }
+
+        const resourceCards = filteredData.map(filteredResource => (
+            <ResourceCard
+                title={filteredResource.title}
+                resource_type={filteredResource.resource_type}
+                description={filteredResource.description}
+                published_date={filteredResource.published_date}
+                environment_topic={filteredResource.environment_topic}
+                grade_level={filteredResource.grade_level}
+            />
+        ));
+
+        setFilteredResources(resourceCards);
+    }, [selectedResourceType, selectedEnvironmentTopic, selectedGradeLevel])
 
     return (
         <div>
             <h1>Resource Library</h1>   
-            <Filter resourceTypes={resourceTypes} setSelectedResourceType={setSelectedResourceType} />    
+            <Filter 
+                resourceTypes={resourceTypes} setSelectedResourceType={setSelectedResourceType}
+                environmentTopics={environment_topics} setSelectedEnvironmentTopic={setSelectedEnvironmentTopic}
+                gradeLevels={grade_levels} setSelectedGradeLevel={setSelectedGradeLevel}
+            />    
             {filteredResources}
         </div>
     );
