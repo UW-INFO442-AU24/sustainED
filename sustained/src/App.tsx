cfaaf4951a755f908abbffa4ee31f4a33cdf937c
zowee101 <zaquino@uw.edu>
2024-11-12 13:32:09 -0800

    fixed routing and installed bootstrap

--- sustained/src/App.tsx
+++ sustained/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import QuizIntro from './components/quiz/QuizIntro';
-import QuizForm from './components/quiz/QuizForm';
+import QuizForm from './components/quiz/quizform';
 import QuizResults from './components/quiz/QuizResults';
 import Library from './components/resources/Library';
 import Register from './components/Register/register'; 
@@ -22,7 +22,6 @@ function App() {
             <Router>
               <Navbar />
               <Routes>
-                <Route path='/' element={<Navigate to="/home" />} /> {/* when npm start is ran, the default page shown is the home component */}
                 <Route path='/home' element={<Home />} />
                 <Route path='/register' element={<Register />} />
                 <Route path='/library' element={<Library />} />
@@ -30,6 +29,7 @@ function App() {
                 <Route path="/quiz-form" element={<QuizForm />} />
                 <Route path="/quiz-results" element={<QuizResults />} />
                 <Route path="/events" element={<EventPage />} />
+                <Route path='/' element={<Navigate to="/home" />} /> {/* when npm start is ran, the default page shown is the home component */}
               </Routes>
             </Router>
           </NextUIProvider>
