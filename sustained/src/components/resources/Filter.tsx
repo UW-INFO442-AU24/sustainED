1684064b4e7bba18980a3f1f3478b3d037cd60da
jtng189 <jtan189@uw.edu>
2024-11-14 10:50:14 -0800

    added mobile css code

--- sustained/src/components/resources/Filter.tsx
+++ sustained/src/components/resources/Filter.tsx
@@ -44,11 +44,11 @@ export default function Filter(props: FilterData) {
     };
 
     return (
-        <div>
+        <div className='filter-box'>
             <h2>Filter By:</h2>
             {/* Resource Types */}
             <label htmlFor="resourceTypeFilter">Resource Type: </label>
-            <select id="resourceTypeFilter" onChange={handleResourceChange} >
+            <select className='filter-option' id="resourceTypeFilter" onChange={handleResourceChange} >
                 <option value="">All</option>
                 {resourceTypes.sort().map((type) => (
                     <option key={type} value={type}>
@@ -58,7 +58,7 @@ export default function Filter(props: FilterData) {
             </select>
             {/* Environment Topics */}
             <label htmlFor="environmentTopicFilter">Environment Topic: </label>
-            <select id="environmentTopicFilter" onChange={handleEnvironmentChange} >
+            <select className="filter-option" id="environmentTopicFilter" onChange={handleEnvironmentChange} >
                 <option value="">All</option>
                 {environmentTopics.sort().map((topic) => (
                     <option key={topic} value={topic}>
@@ -68,7 +68,7 @@ export default function Filter(props: FilterData) {
             </select>
             {/* Grade levels */}
             <label htmlFor="gradeLevelFilter">Grade Level Comprehension: </label>
-            <select id="gradeLevelFilter" onChange={handleGradeLevelChange} >
+            <select className="filter-option" id="gradeLevelFilter" onChange={handleGradeLevelChange} >
                 <option value="0">All</option>
                 {gradeLevels.sort((a, b) => a - b).map((level) => (
                     <option key={level} value={level}>
@@ -77,13 +77,13 @@ export default function Filter(props: FilterData) {
                 ))}
             </select>
             {/* search component */}
-            <label htmlFor="search">Search: </label>
-            <input type="text" id="search" name="library_search"
+            {/* <label htmlFor="search">Search: </label> */}
+            <input className="filter-option" type="text" id="search" name="library_search"
                 placeholder='Search by title'
                 onChange={handleSearch}
             />
             
-            <button type="button" onClick={submitFilter}>Filter</button>
+            <button className='filter-resources' type="button" onClick={submitFilter}>Filter</button>
         </div>
     );
 }
